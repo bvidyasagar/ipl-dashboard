a8c00e746f26b49ae8fd425519c1fc75a86928a3
Bandi, Vidya Sagar <vidyasagar.bandi@oracle.com>
2025-08-03 20:11:04 +0530

    Updating the latest changes to be in sync

--- ipl-dashboard/src/main/java/com/sagar/ipl_dashboard/controller/TeamController.java
+++ ipl-dashboard/src/main/java/com/sagar/ipl_dashboard/controller/TeamController.java
@@ -2,6 +2,7 @@ package com.sagar.ipl_dashboard.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -11,6 +12,7 @@ import com.sagar.ipl_dashboard.service.TeamService;
 
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     @Autowired
